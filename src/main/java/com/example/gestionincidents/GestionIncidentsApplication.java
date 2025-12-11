57594a81bd6108aa66469bdd18ed65fdc6d0ba06
Kmar Mlayhi <kmar.mlayhi3@gmail.com>
2025-12-11 11:45:38 +0100

    Initial commit

---
+++ src/main/java/com/example/gestionincidents/GestionIncidentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionincidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionIncidentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionIncidentsApplication.class, args);
+    }
+
+}
